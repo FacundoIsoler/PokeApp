b937ac460fb80feebb50a6d8263b30835f0260e1
Facundo Isoler <facundoisoler@outlook.com>
2022-10-27 23:07:57 -0300

    tercer commit

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,18 +1,58 @@
 const { Router } = require('express');
+const axios = require('axios');
 const router = Router();
 const { Pokemon } = require('../db')
+
+router.get('/api', (req, res, next) => {
+    let pokemonPromesaApi = axios.get(' https://pokeapi.co/api/v2/pokemon');
+    let pokemonPromesaDb = Pokemon.findAll();
+    Promise.all([pokemonPromesaApi, pokemonPromesaDb])
+        .then((respuesta) => {
+            const [pokemonApi, pokemonDb] = respuesta;
+            let mapeoApi = pokemonApi.data.results.map((pokemon) => {
+                return {
+                    name: pokemon.name,
+                    url: pokemon.url
+                }
+            })
+            let todosLosPokemons = [...mapeoApi, ...pokemonDb]
+            res.send(todosLosPokemons);
+        })
+});
 
 router.get('/', (req, res, next) => {
     return Pokemon.findAll()
         .then((pokemons) => {
             res.send(pokemons);
         })
+        .catch((err) => {
+            next(err);
+        });
 });
 
+// GET /pokemons/{idPokemon}
+
+router.get('/pokemons/{idPokemon}', async (req, res, next) => {
+    let { idPokemon } = req.params.id;
+    const character = Pokemon.findByPk(pokemons[idPokemon])
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+//[ ] GET /pokemons?name="...":
+
+
 router.post('/', async (req, res, next) => {
-    const { name, vida, ataque, defensa, velocidad, altura, peso } = req.body
-    const newPokemon = await Pokemon.create({name, vida, ataque, defensa, velocidad, altura, peso})
-    return res.send(newPokemon)
+    try {
+        const { name, vida, ataque, defensa, velocidad, altura, peso } = req.body
+        const newPokemon = await Pokemon.create({ name, vida, ataque, defensa, velocidad, altura, peso })
+        return res.status(200).send(newPokemon)
+    } catch (error) {
+        next(error)
+    }
 });
 
 router.put('/', (req, res, next) => {
@@ -24,7 +64,7 @@ router.delete('/', (req, res, next) => {
 });
 
 
-// GET /pokemons/{idPokemon}
+
 
 
 
